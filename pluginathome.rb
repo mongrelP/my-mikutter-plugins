321fcb87915dcf67a0bd1ee5d443a4d955dd2690
mongrelP <kimi.nicoco@gmail.com>
2011-09-16 17:19:15 +0900

    change path ~/.mikutter_plagin to ~/.mikutter/plagins

--- pluginathome.rb
+++ pluginathome.rb
@@ -2,7 +2,7 @@
 # mikutterのpluginディレクトリ以下に配置すると~/mikutter_plugin以下の
 # プラグインを読み込む
 
-dir = File.expand_path("~/.mikutter_plugin")
+dir = File.expand_path("~/.mikutter/plugins")
 if FileTest.directory?(dir)
     Dir.glob(dir + "/*.rb").each do |rb|
         require rb
